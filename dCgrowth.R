1fca3c8bfbe596a16915eb88340d25c9924e3348
michellelam777 <michellelam777@gmail.com>
2023-05-22 17:53:47 -0700

    adding comments

--- dCgrowth.R
+++ dCgrowth.R
@@ -1,11 +1,11 @@
 #'  Forest growth derivative
 #' @param time time since start
-#' @param C size of forest
-#' @param Ct canopy closure threshold
+#' @param C size of forest (kgC)
+#' @param Ct canopy closure threshold (kgC)
 #' @param parms - as list with two to three values, r, g, K
 #' @param r early exponential growth rate
-#' @param g linear growth rate
-#' @param K carrying capacity
+#' @param g linear growth rate (kg/year)
+#' @param K carrying capacity (kgC)
 #' @return derivative of carbon with time
 
 dCgrowth = function(Time, C, parms) {
